b3da2979863427f3dc1efd1caab0e36f37382c1d
jjdone <ehdlfaos@gmail.com>
2023-01-20 16:03:00 +0900

    Member 생성

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void save() throws Exception {
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member saveMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> result = memberRepository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
